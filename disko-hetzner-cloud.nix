702ad919a0894b2dd2323946179e231e8d18ee55
guillem.cordoba <guillem.cordoba@gmail.com>
2025-02-21 13:04:13 +0100

    Attempt deploy

---
+++ disko-hetzner-cloud.nix
@@ -0,0 +1,32 @@
+{ disks ? [ "/dev/sda" ], ... }: {
+  disko.devices = {
+    disk = {
+      sda = {
+        device = builtins.elemAt disks 0;
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "1000M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
--- flake.nix
+++ flake.nix
@@ -2,56 +2,102 @@
   description = "";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     cachix-deploy-flake.url = "github:cachix/cachix-deploy-flake";
+    srvos = {
+      url = "github:numtide/srvos";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    demo-launcher.url = "github:darksoil-studio/demo-launcher";
+
   };
 
-  outputs = { nixpkgs, cachix-deploy-flake, ... }: 
+  outputs = { nixpkgs, cachix-deploy-flake, srvos, disko, demo-launcher, ... }:
     let
       # change these 
-      machineName = "myagent";
-      sshPubKey = "ssh-rsa XXX me@machine";
+      machineName = "file-storage-provider-aon";
+      sshPubKeys = {
+        guillem =
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDTE+RwRfcG3UNTOZwGmQOKd5R+9jN0adH4BIaZvmWjO guillem.cordoba@gmail.com";
+      };
 
       lib = nixpkgs.lib;
       forAllSystems = lib.genAttrs lib.systems.flakeExposed;
       common = system: rec {
         pkgs = nixpkgs.legacyPackages.${system};
         cachix-deploy-lib = cachix-deploy-flake.lib pkgs;
-        bootstrapNixOS = cachix-deploy-lib.bootstrapNixOS { 
-          system = system; 
-          hostname = machineName;
-          grubDevices = [ "/dev/nvme0n1"  "/dev/nvme1n1" ];
-          sshPubKey = sshPubKey;
-        };
+        modules = [
+          srvos.nixosModules.hardware-hetzner-cloud
+          srvos.nixosModules.server
+          srvos.nixosModules.mixins-systemd-boot
+          disko.nixosModules.disko
+          (import ./disko-hetzner-cloud.nix { disks = [ "/dev/sda" ]; })
+          ({
+            system.stateVersion = "24.11";
+
+            services.cachix-agent.enable = true;
+            # boot.loader.efi.canTouchEfiVariables = true;
+            networking.hostName = machineName;
+            users.users.root.openssh.authorizedKeys.keys =
+              builtins.attrValues sshPubKeys;
+            services.openssh.settings.PermitRootLogin = "without-password";
+          })
+        ];
+        bootstrapNixOS = lib.nixosSystem { inherit system modules; };
+        # bootstrapNixOS = cachix-deploy-lib.bootstrapNixOS {
+        #   system = system;
+        #   hostname = machineName;
+        #   sshPubKey = sshPubKey;
+        #   grubDevices = [ "/dev/sda" ];
+        #   diskoDevices = import ./disko-hetzner-cloud.nix { inherit lib; };
+        # };
       };
     in {
-      nixosConfigurations.${machineName} = (common "x86_64-linux").bootstrapNixOS.nixos;
+      nixosConfigurations.${machineName} =
+        (common "x86_64-linux").bootstrapNixOS;
 
-      packages = forAllSystems (system: 
-        let 
-          inherit (common system) pkgs cachix-deploy-lib bootstrapNixOS;
+      packages = forAllSystems (system:
+        let inherit (common system) cachix-deploy-lib modules;
         in {
-        default = cachix-deploy-lib.spec {
+          default = cachix-deploy-lib.spec {
             agents = {
               "${machineName}" = cachix-deploy-lib.nixos {
-                imports = [ bootstrapNixOS.module ];
+                imports = modules;
 
                 config = {
                   # here comes all your NixOS configuration
+                  systemd.services.aon = let
+                    aon =
+                      demo-launcher.outputs.packages."x86_64-linux".file-storage-provider;
+                  in {
+                    enable = true;
+                    path = [ aon ];
+                    wantedBy = [ "multi-user.target" ];
+                    serviceConfig = {
+                      ExecStart =
+                        "${aon}/bin/always-online-node --data-dir /root --local-only";
+                      Restart = "always";
+                      RestartSec = 10;
+                    };
+                  };
+
                 };
               };
             };
           };
         });
-          
-      devShells = forAllSystems (system: 
-        let 
-          inherit (common system) pkgs;
-        in { 
-        default = pkgs.mkShell {
-          buildInputs = [
-            cachix-deploy-flake.packages.${system}.bootstrapHetzner
-          ];
-        };
-      });
-  }; 
+
+      devShells = forAllSystems (system:
+        let inherit (common system) pkgs;
+        in {
+          default = pkgs.mkShell {
+            buildInputs =
+              [ cachix-deploy-flake.packages.${system}.bootstrapHetzner ];
+          };
+        });
+    };
 }
