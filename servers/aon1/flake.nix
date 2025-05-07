1eea6fc93aca8d2852dc1680b2a3d1028766ef0d
guillem.cordoba <guillem.cordoba@gmail.com>
2025-05-07 16:55:11 +0200

    Update aons

--- servers/aon1/flake.nix
+++ servers/aon1/flake.nix
@@ -13,8 +13,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     aon.url = "github:darksoil-studio/always-online-nodes";
-    demo-launcher.url = "github:darksoil-studio/demo-launcher/v0.2.3";
-    dash-chat.url = "github:darksoil-studio/dash-chat/v0.2.2";
+    demo-launcher.url = "github:darksoil-studio/demo-launcher/v0.2.5";
+    dash-chat.url = "github:darksoil-studio/dash-chat/v0.2.3";
   };
 
   outputs = inputs@{ nixpkgs, cachix-deploy-flake, srvos, disko, ... }:
